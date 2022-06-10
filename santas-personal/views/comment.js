5b587e4fe2125869483f67842252813d9e8d1b58
naenae0210 <105160125+naenae0210@users.noreply.github.com>
2022-06-10 19:31:44 +0900

    Add files via upload

---
+++ santas-personal/views/comment.js
@@ -0,0 +1,34 @@
+const Post = require('./post');
+const User = require('./user');
+
+module.exports = (sequelize, Sequelize) => {
+  const comment = sequelize.define('comment', {
+    commentNum: {
+      type: Sequelize.INTEGER,
+      allowNull: false,
+      autoIncrement: true,
+      primaryKey: true
+    },
+    postId: {
+      type: Sequelize.INTEGER,
+      allowNull: false,
+      references: {
+        model: 'posts',
+        key: 'id',
+        deferrable: Sequelize.Deferrable.INITIALLY_IMMEDIATE
+      }
+    },
+    commentDetail: {
+      type: Sequelize.STRING,
+      allowNull: false
+    },
+
+  });
+
+  comment.associate = function (models) {
+    comment.belongsTo(models.post, {
+      foreignKey: "postId"
+    })
+  };
+  return comment;
+}
